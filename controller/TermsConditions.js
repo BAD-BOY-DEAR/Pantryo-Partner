6c571d69f739b11f5d7102db2627e55e0c61186a
sidmrlazy <59534733+sidmrlazy@users.noreply.github.com>
2021-05-17 11:48:43 +0530

    Checking Development Branch Commits

--- controller/TermsConditions.js
+++ controller/TermsConditions.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 const TermsConditions = () => {
   return (
     <View>
-      <Text>TermsConditions</Text>
+      <Text>Terms & Conditions</Text>
     </View>
   );
 };
