40df0dc4f62e8648c6e19adbe38a226c696e23b1
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2021-08-31 12:53:30 +0530

    Changed UI of AddProduct.js Screen

--- view/Inventory/Product/AddProduct.js
+++ view/Inventory/Product/AddProduct.js
@@ -304,11 +304,15 @@ const AddProducts = ({route, navigation}) => {
         <LoaderScreen />
       ) : (
         <View style={styles.container}>
+          {/* ======== Header Add Button ======== */}
           <View style={styles.header}>
             <Pressable style={styles.headerBtn}>
               <Text style={styles.headerBtnTxt}>Add</Text>
             </Pressable>
           </View>
+          {/* ======== Header Add Button ======== */}
+
+          {/* ======== Search Section ======== */}
           <View style={styles.searchSection}>
             <View style={styles.searchBox}>
               <Icons name="search-outline" size={20} color="#777" />
@@ -324,64 +328,73 @@ const AddProducts = ({route, navigation}) => {
               </Pressable> */}
             </View>
           </View>
-          <View style={styles.catSection}>
+          {/* ======== Search Section ======== */}
+
+          <View
+            style={{
+              width: '100%',
+            }}>
             {pantryoInvetory !== '' ? (
               <FlatList
                 ref={ref}
                 showsVerticalScrollIndicator={false}
                 ListHeaderComponent={() => (
-                  <View style={styles.catContainer}>
-                    {mainCategoryName == 'Spices & Masala' ? (
-                      <Image source={masala} style={styles.catImg} />
-                    ) : mainCategoryName == 'Edible Oils' ? (
-                      <Image source={edibleOils} style={styles.catImg} />
-                    ) : mainCategoryName == 'Wheat Flour' ? (
-                      <Image source={attaImg} style={styles.catImg} />
-                    ) : mainCategoryName == 'Besan' ? (
-                      <Image source={besan} style={styles.catImg} />
-                    ) : mainCategoryName == 'Flour' ? (
-                      <Image source={flour} style={styles.catImg} />
-                    ) : mainCategoryName == 'Sooji' ? (
-                      <Image source={sooji} style={styles.catImg} />
-                    ) : mainCategoryName == 'Rice Flour' ? (
-                      <Image source={riceFlour} style={styles.catImg} />
-                    ) : mainCategoryName == 'Other Flours' ? (
-                      <Image source={otherFlour} style={styles.catImg} />
-                    ) : mainCategoryName == 'Rice' ? (
-                      <Image source={rice} style={styles.catImg} />
-                    ) : mainCategoryName == 'Salt & Sugar' ? (
-                      <Image source={saltSugar} style={styles.catImg} />
-                    ) : mainCategoryName == 'Pulses & Grains' ? (
-                      <Image source={pulsesGrains} style={styles.catImg} />
-                    ) : mainCategoryName == 'Baking Items' ? (
-                      <Image source={baking} style={styles.catImg} />
-                    ) : mainCategoryName == 'Frozen Food' ? (
-                      <Image source={frozenFood} style={styles.catImg} />
-                    ) : mainCategoryName == 'Packaged Products' ? (
-                      <Image source={packaged} style={styles.catImg} />
-                    ) : mainCategoryName == 'Vegetables' ? (
-                      <Image source={veg} style={styles.catImg} />
-                    ) : mainCategoryName == 'Fruits' ? (
-                      <Image source={fruits} style={styles.catImg} />
-                    ) : (
-                      <Icons name="image" size={40} color="#777" />
-                    )}
-                    <View
-                      style={{
-                        flex: 1,
-                        marginLeft: 20,
-                      }}>
-                      <Text style={styles.catName}>{mainCategoryName}</Text>
-                      <Text style={styles.noticeTxt}>
-                        Select all the items that you sell from your store. You
-                        can also edit the price & the Qty of the items
-                        accordingly
-                      </Text>
+                  <>
+                    {/* ======== Category Image Section ======== */}
+                    <View style={styles.catContainer}>
+                      {mainCategoryName == 'Spices & Masala' ? (
+                        <Image source={masala} style={styles.catImg} />
+                      ) : mainCategoryName == 'Edible Oils' ? (
+                        <Image source={edibleOils} style={styles.catImg} />
+                      ) : mainCategoryName == 'Wheat Flour' ? (
+                        <Image source={attaImg} style={styles.catImg} />
+                      ) : mainCategoryName == 'Besan' ? (
+                        <Image source={besan} style={styles.catImg} />
+                      ) : mainCategoryName == 'Flour' ? (
+                        <Image source={flour} style={styles.catImg} />
+                      ) : mainCategoryName == 'Sooji' ? (
+                        <Image source={sooji} style={styles.catImg} />
+                      ) : mainCategoryName == 'Rice Flour' ? (
+                        <Image source={riceFlour} style={styles.catImg} />
+                      ) : mainCategoryName == 'Other Flours' ? (
+                        <Image source={otherFlour} style={styles.catImg} />
+                      ) : mainCategoryName == 'Rice' ? (
+                        <Image source={rice} style={styles.catImg} />
+                      ) : mainCategoryName == 'Salt & Sugar' ? (
+                        <Image source={saltSugar} style={styles.catImg} />
+                      ) : mainCategoryName == 'Pulses & Grains' ? (
+                        <Image source={pulsesGrains} style={styles.catImg} />
+                      ) : mainCategoryName == 'Baking Items' ? (
+                        <Image source={baking} style={styles.catImg} />
+                      ) : mainCategoryName == 'Frozen Food' ? (
+                        <Image source={frozenFood} style={styles.catImg} />
+                      ) : mainCategoryName == 'Packaged Products' ? (
+                        <Image source={packaged} style={styles.catImg} />
+                      ) : mainCategoryName == 'Vegetables' ? (
+                        <Image source={veg} style={styles.catImg} />
+                      ) : mainCategoryName == 'Fruits' ? (
+                        <Image source={fruits} style={styles.catImg} />
+                      ) : (
+                        <Icons name="image" size={40} color="#777" />
+                      )}
+                      <View
+                        style={{
+                          flex: 1,
+                          marginLeft: 20,
+                        }}>
+                        <Text style={styles.catName}>{mainCategoryName}</Text>
+                        <Text style={styles.noticeTxt}>
+                          Select all the items that you sell from your store.
+                          You can also edit the price & the Qty of the items
+                          accordingly
+                        </Text>
+                      </View>
                     </View>
-                  </View>
+                    {/* ======== Category Image Section ======== */}
+                  </>
                 )}
                 nestedScrollEnabled={true}
-                style={{width: '100%'}}
+                style={{width: '100%', marginBottom: 40}}
                 data={pantryoInvetory}
                 refreshControl={
                   <RefreshControl
@@ -391,7 +404,99 @@ const AddProducts = ({route, navigation}) => {
                 }
                 renderItem={({item, index}) => (
                   <>
-                    {item.product_assign_status == 'not added' ? (
+                    {/* ========= Products Start ========= */}
+                    <View style={styles.productContainer}>
+                      <View style={styles.productRow}>
+                        {/* ========= Products Details Start ========= */}
+                        <Text style={styles.productBrand}>
+                          {item.pantryo_brand_name
+                            ? item.pantryo_brand_name
+                            : ''}
+                        </Text>
+                        <Text style={styles.productMain}>
+                          {item.pantryo_item_name
+                            ? item.pantryo_item_name
+                            : 'Unable to fetch product name'}
+                        </Text>
+                        {/* ========= Products Details End ========= */}
+
+                        {/* ========= Products Qty/Price/Unit Start ========= */}
+                        <View style={styles.productInnerRow}>
+                          <TextInput
+                            placeholderTextColor="#000"
+                            placeholder="Quantity"
+                            keyboardType="number-pad"
+                            value={
+                              item.pantryo_inventory_id === inventoryId
+                                ? inventoryQty
+                                : item.pantryo_item_qty !== ''
+                                ? item.pantryo_item_qty
+                                : 'No'
+                            }
+                            onChangeText={text => {
+                              setInventoryQty(text);
+                              setInventoryId(item.pantryo_inventory_id);
+                            }}
+                            style={styles.productInput}
+                          />
+
+                          <View
+                            style={{
+                              flex: 1,
+                              borderWidth: 0.5,
+                              borderColor: '#c7c7c7c7',
+                            }}>
+                            <Picker
+                              mode="dropdown"
+                              style={{
+                                width: '80%',
+                                justifyContent: 'flex-end',
+                                textAlign: 'center',
+                              }}
+                              onValueChange={(itemValue, itemIndex) => {
+                                setSelectedUnit(itemValue);
+                                setInventoryId(item.pantryo_inventory_id);
+                              }}>
+                              <Picker.Item label="gm" value="gm" />
+                              <Picker.Item label="kg" value="kg" />
+                              <Picker.Item label="ml" value="ml" />
+                              <Picker.Item label="ltr" value="ltr" />
+                              <Picker.Item label="pcs" value="pcs" />
+                              <Picker.Item label="crate" value="crate" />
+                              <Picker.Item label="Dozen" value="Dozen" />
+                            </Picker>
+                          </View>
+
+                          <TextInput
+                            placeholder="₹"
+                            placeholderTextColor="#777"
+                            keyboardType="number-pad"
+                            value={
+                              item.pantryo_inventory_id === inventoryId
+                                ? newprice
+                                : item.pantryo_item_price
+                            }
+                            style={styles.productInput}
+                            onChangeText={text => {
+                              setNewPrice(text);
+                              setInventoryId(item.pantryo_inventory_id);
+                            }}
+                          />
+                        </View>
+                        {/* ========= Products Qty/Price/Unit End ========= */}
+                      </View>
+                      <View style={styles.productDiv}>
+                        <CheckBox
+                          disabled={false}
+                          value={toggleCheckBox}
+                          onValueChange={newValue =>
+                            setToggleCheckBox(newValue)
+                          }
+                        />
+                      </View>
+                    </View>
+                    {/* ========= Products Start ========= */}
+                    {/* {item.product_assign_status == 'not added' ? (
                       <View style={styles.prodTab}>
                         <View style={styles.productContainer}>
                           <View style={styles.productDiv}>
@@ -564,7 +669,7 @@ const AddProducts = ({route, navigation}) => {
                           </Pressable>
                         )}
                       </View>
-                    )}
+                    )} */}
                   </>
                 )}
                 keyExtractor={(item, pantryo_inventory_id) =>
@@ -616,10 +721,7 @@ const styles = StyleSheet.create({
     padding: 3,
     borderRadius: 5,
   },
-  catSection: {
-    width: '100%',
-    marginBottom: 30,
-  },
+
   catContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
@@ -661,16 +763,16 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
     marginBottom: 20,
   },
-  productContainer: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    paddingBottom: 10,
-    marginTop: 10,
-    marginBottom: 30,
-  },
+  // productContainer: {
+  //   flexDirection: 'row',
+  //   width: '100%',
+  //   justifyContent: 'flex-start',
+  //   alignItems: 'center',
+  //   paddingHorizontal: 20,
+  //   paddingBottom: 10,
+  //   marginTop: 10,
+  //   marginBottom: 30,
+  // },
   productDiv: {
     flex: 3,
   },
@@ -732,5 +834,58 @@ const styles = StyleSheet.create({
     fontFamily: 'OpenSans-SemiBold',
     fontSize: 18,
     color: '#fff',
+  },
+  // New FlatList STyle
+  productContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#c7c7c7c7',
+    paddingVertical: 20,
+  },
+  productRow: {
+    width: '100%',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    flex: 1,
+    marginRight: 15,
+  },
+  productBrand: {
+    fontFamily: 'OpenSans-Medium',
+    fontSize: 16,
+  },
+  productMain: {
+    fontFamily: 'OpenSans-SemiBold',
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  productInnerRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  productQty: {
+    fontFamily: 'OpenSans-SemiBold',
+    fontSize: 16,
+    marginRight: 10,
+  },
+  productPrice: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 18,
+    color: 'green',
+  },
+  productDiv: {
+    marginRight: 10,
+  },
+  productInput: {
+    borderWidth: 0.5,
+    flex: 1,
+    paddingHorizontal: 10,
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 16,
+    borderColor: '#c7c7c7c7',
   },
 });
