c28d20ed9409a1fe90838ed190202fb0dbd7ca77
SHIV YADAV <71929825+BAD-BOY-DEAR@users.noreply.github.com>
2021-09-10 17:22:38 +0530

    push

--- view/HomeScreen/HomeScreen.js
+++ view/HomeScreen/HomeScreen.js
@@ -355,7 +355,7 @@ const HomeScreen = ({navigation}) => {
   }
 
   useEffect(() => {
-    // console.log('Hii');
+    console.log('Hii');
     LogBox.ignoreAllLogs(true);
     LogBox.ignoreLogs(['Warning: ...']);
     LogBox.ignoreLogs(['VirtualizedLists should never be nested...']);
